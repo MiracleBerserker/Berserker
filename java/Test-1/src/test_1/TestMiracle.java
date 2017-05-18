42bb8d3d010560bf1cb37173f464f2f2432de81f
miracle <873144200@qq.com>
2017-05-18 17:36:54 +0800

    前端练习 2017-5-18

---
+++ java/Test-1/src/test_1/TestMiracle.java
@@ -0,0 +1,15 @@
+package test_1;
+
+import org.junit.Test;
+
+public class TestMiracle {
+
+	@Test
+	public void test_1(){
+		int i=0;
+		for(i=0;i<10;i++){
+			System.out.println("========:  "+i);
+		}
+		System.out.println(i);
+	}
+}
